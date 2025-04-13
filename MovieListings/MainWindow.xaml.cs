7095db6dc12653363a26e47a0bf49faa7c6d7619
Garry Ledwith <S00256356@atu.ie>
2025-04-13 17:48:03 +0000

    working on booking tickets for movie

--- MovieListings/MainWindow.xaml.cs
+++ MovieListings/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace MovieListings
     /// </summary>
     public partial class MainWindow : Window
     {
+        MovieData db = new MovieData();
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,5 +33,77 @@ namespace MovieListings
         {
 
         }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime selectedDate = dpshowMovie.SelectedDate.Value;
+
+            if (selectedDate != null)
+            {
+                SearchByDate(selectedDate); // calls the search by date method 
+            }
+        }
+
+
+        // searrch database by selected date 
+        public void SearchByDate(DateTime selectedDate)
+        {
+            try
+            {
+                // LINQ for database query 
+                var query = from b in db.Bookings
+                            where b.BookingDate == selectedDate
+                            select b;
+                var results = query.ToList();
+
+                // check if booking is on the selected date, and update 
+                if (results.Count > 0)
+                {
+                    // update list box 
+                    lbxMovieListings.ItemsSource = results; // set the list box items to the results from the database query above 
+
+                    // count the total number of tickets sold for movie 
+                    int totalTicketsSold = results.Sum(p => p.NumberOfTickets);
+
+                    // get required tickets 
+                    int requiredTickets = int.Parse(tbxRequiredSeats.Text);
+                    
+                    // get current abailable seats 
+                    int currentAvailableSeats = (100 - totalTicketsSold);
+
+                    if (currentAvailableSeats >= requiredTickets)
+                    {
+                        tblkAvailableSeats.Text = currentAvailableSeats.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sold Out");
+                        // return from here? 
+                    }
+                   
+                    
+
+                    // update abailability (total capacity - total participants) 
+                    tblkAvailableSeats.Text = (100 - requiredTickets) + "%";
+
+                }
+                else
+                {
+                    // refresh screen if no result is found 
+                    //RefreshScreen();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, error occured when trying to connect to database" + ex.Message);
+            }
+        }
+
+        private void lbxMovieListings_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        
     }
 }
--- MovieListings/MovieData.cs
+++ MovieListings/MovieData.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace MovieListings
 {
-    public class MovieData 
+    public class MovieData : DbContext
     {
+        public MovieData() :base("2023OODExam_GarryLedwith") { }
+
+        public DbSet<Movie> Movies { get; set;}
+        public DbSet<Booking> Bookings { get; set;}
+
     }
 }
